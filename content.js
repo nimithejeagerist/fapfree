fa5c88ade0076c18519679d9834a915f2934284d
Nimi Akinroye <takinroy@ualberta.ca>
2024-05-18 02:22:18 -0600

    we will be restarting again without help from that AI, i have gained clarity

---
+++ content.js
@@ -0,0 +1,18 @@
+// List of blocked websites
+const blockedSites = [
+    "www.pornhub.com",
+    "www.xvideos.com",
+    "www.xhamster.com",
+    "www.xnxx.com",
+    "www.eporner.com",
+    "www.hqporner.com",
+    "www.beeg.com",
+    "www.sxyprn.com",
+    "www.spankbang.com",
+    "www.porntex.com",
+    "www.fapello.com",
+    "www.erothots.co",
+    "www.thothub.to",
+    "www.nudostar.com",
+    "www.onlyfans.com"
+];
